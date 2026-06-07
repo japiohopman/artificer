d4647d960407954a554d151de2ebf3e363aa24d5
jaap Hopman <94796592+japiohopman@users.noreply.github.com>
2026-06-07 14:27:59 +0200

    Add dice roller components to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@
  */
 
 import ArcaneCodex from './components/ArcaneCodex';
+import { DiceBoxCanvas } from './dice_roller/DiceBoxCanvas';
+import { DiceRollOverlay } from './dice_roller/DiceRollOverlay';
+import { AdvancedRoller } from './components/DiceRollerPanel';
 
 export default function App() {
   return (
     <div className="min-h-screen">
       <ArcaneCodex />
+      
+      {/* Global Dice Layer */}
+      <DiceRollOverlay />
+      <DiceBoxCanvas />
+      <AdvancedRoller />
     </div>
   );
 }
